16f3cebed87da4327083f27c460023c840455e72
Meet <gandhimeet81104@gmail.com>
2023-09-28 11:42:35 +0530

    Let's Make BlackJack Game

--- b.js
+++ b.js
@@ -1,13 +1,17 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
+    isAlive = false
 }
-
-
+console.log("You Can withdraw" + " "+ hasBlackJack)
+console.log("Or You Can Continue"+" "+isAlive)
